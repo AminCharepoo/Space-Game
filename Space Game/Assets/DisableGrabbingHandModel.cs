d75399f412c01a040ca2537f05b1e43b67ad4910
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2024-10-08 11:24:00 -0400

    remove hand model

--- Space Game/Assets/DisableGrabbingHandModel.cs
+++ Space Game/Assets/DisableGrabbingHandModel.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-
 public class DisableGrabbingHandModel : MonoBehaviour
 {
     public GameObject leftHandModel;
     public GameObject rightHandModel;
+
     // Start is called before the first frame update
     void Start()
     {
-        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();;
         grabInteractable.selectEntered.AddListener(HideGrabbingHand);
-        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+        
     }
 
     public void HideGrabbingHand(SelectEnterEventArgs args)
     {
-        if (args.interactorObject.transform.tag == "Left Hand")
+        if(args.interactorObject.transform.tag == "Left Hand")
         {
             leftHandModel.SetActive(false);
         }
-        else if (args.interactorObject.transform.tag == "Right Hand")
+        else if(args.interactorObject.transform.tag == "Right Hand")
         {
             rightHandModel.SetActive(false);
         }
@@ -31,18 +31,20 @@ public class DisableGrabbingHandModel : MonoBehaviour
 
     public void ShowGrabbingHand(SelectExitEventArgs args)
     {
-        if (args.interactorObject.transform.tag == "Left Hand")
+        if(args.interactorObject.transform.tag == "Left Hand")
         {
             leftHandModel.SetActive(true);
         }
-        else if (args.interactorObject.transform.tag == "Right Hand")
+        else if(args.interactorObject.transform.tag == "Right Hand")
         {
             rightHandModel.SetActive(true);
         }
     }
+
+
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
